251c70c874e5f5ea930301d8ec1916a3077118ec
Durgesh Singh <67506485+erdksingh@users.noreply.github.com>
2022-07-04 11:23:28 +1000

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Main from "./containers/Main";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("DK-FOLIO-000000-01");
+    ReactGA.pageview(window.location.pathname);
+  });
   return (
     <div>
       <Main />
