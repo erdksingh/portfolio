0c3d823774a2a16a293435e59917beff0481cee5
Durgesh Singh <67506485+erdksingh@users.noreply.github.com>
2022-07-04 11:23:53 +1000

    Updates

---
+++ precache-manifest.c37b5cdee637310252c5a436f87db5cd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "777cee88c3b1e262cc79",
+    "url": "/portfolio/static/css/main.468e6a1f.chunk.css"
+  },
+  {
+    "revision": "777cee88c3b1e262cc79",
+    "url": "/portfolio/static/js/main.777cee88.chunk.js"
+  },
+  {
+    "revision": "a9e9ef35c945387be1d2",
+    "url": "/portfolio/static/js/1.a9e9ef35.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "cfb52fa3f465f0d6d6b653c399d2940d",
+    "url": "/portfolio/static/media/college.cfb52fa3.png"
+  },
+  {
+    "revision": "cae2b4090926b360df8f8147f203269e",
+    "url": "/portfolio/static/media/edulabs.cae2b409.png"
+  },
+  {
+    "revision": "bfcde136650214271f101e8877e74486",
+    "url": "/portfolio/static/media/user.bfcde136.jpg"
+  },
+  {
+    "revision": "df6d9cb1b119ecf124b7e3307ca5858f",
+    "url": "/portfolio/static/media/tpg.df6d9cb1.jpg"
+  },
+  {
+    "revision": "5c45fd93e157227bda583669c9b29854",
+    "url": "/portfolio/static/media/instaAward1.5c45fd93.jpg"
+  },
+  {
+    "revision": "07f7d3e668e7101ce5b729c32dfaa895",
+    "url": "/portfolio/static/media/instaAward2.07f7d3e6.jpg"
+  },
+  {
+    "revision": "9a224c68c5eecd4b4ca1a8c63fb1d8a9",
+    "url": "/portfolio/static/media/instaAward3.9a224c68.jpg"
+  },
+  {
+    "revision": "6c0d632e73c36ddfdd764d5f0b81b157",
+    "url": "/portfolio/static/media/instaAward4.6c0d632e.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "836766c6a44cb7c2ea59468566031084",
+    "url": "/portfolio/index.html"
+  }
+];
