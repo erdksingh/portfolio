6e6dfaa69b90d4b794726d9c0fcb95c3921ba8f0
Durgesh Singh <67506485+erdksingh@users.noreply.github.com>
2023-12-26 00:19:39 +1100

    Updates

---
+++ precache-manifest.5aa673840a380ce5b889a5f50912860c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b147d54ea626d266dd380d0faea5ac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "966f7e05575831c86b5b",
+    "url": "/portfolio/static/css/main.9dc9a729.chunk.css"
+  },
+  {
+    "revision": "7c68b4fa1af17852e946",
+    "url": "/portfolio/static/js/2.1ab968fb.chunk.js"
+  },
+  {
+    "revision": "966f7e05575831c86b5b",
+    "url": "/portfolio/static/js/main.69c275d2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "cfb52fa3f465f0d6d6b653c399d2940d",
+    "url": "/portfolio/static/media/college.cfb52fa3.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "cae2b4090926b360df8f8147f203269e",
+    "url": "/portfolio/static/media/edulabs.cae2b409.png"
+  },
+  {
+    "revision": "5c45fd93e157227bda583669c9b29854",
+    "url": "/portfolio/static/media/instaAward1.5c45fd93.jpg"
+  },
+  {
+    "revision": "07f7d3e668e7101ce5b729c32dfaa895",
+    "url": "/portfolio/static/media/instaAward2.07f7d3e6.jpg"
+  },
+  {
+    "revision": "9a224c68c5eecd4b4ca1a8c63fb1d8a9",
+    "url": "/portfolio/static/media/instaAward3.9a224c68.jpg"
+  },
+  {
+    "revision": "6c0d632e73c36ddfdd764d5f0b81b157",
+    "url": "/portfolio/static/media/instaAward4.6c0d632e.jpg"
+  },
+  {
+    "revision": "ff5df6a6d417ce6d2a08e5d2782a1623",
+    "url": "/portfolio/static/media/optus.ff5df6a6.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "df6d9cb1b119ecf124b7e3307ca5858f",
+    "url": "/portfolio/static/media/tpg.df6d9cb1.jpg"
+  },
+  {
+    "revision": "bfcde136650214271f101e8877e74486",
+    "url": "/portfolio/static/media/user.bfcde136.jpg"
+  }
+]);
